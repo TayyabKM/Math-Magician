1ee19b4931918b979c0d6118ef0d421bc79c4820
TayyabKM <109720981+TayyabKM@users.noreply.github.com>
2023-02-24 09:21:27 +0100

    Make required changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Calculator from './components/calculator';
-/* eslint-disable */
-class App extends React.Component {
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
-      <Calculator />
+      <div className="App">
+        <Calculator />
+      </div>
     );
   }
 }
-
-export default App;
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,47 +1,52 @@
 import React from 'react';
-import './calculator.css';
-/* eslint-disable */
-class Calculator extends React.Component {
+
+export default class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
+      <div className="buttons-cont">
+        <div className="calc-section">0</div>
+        <div className="grid-container">
+          <div className="container-row">
+            <button type="button" className="button-grey">AC</button>
+            <button type="button" className="button-grey">+/-</button>
+            <button type="button" className="button-grey">%</button>
+            <button type="button" className="button-orange">÷</button>
+          </div>
 
-      <div className="calculator">
-        <input type="text" className="calculator-screen" value="0" disabled />
-        <div className="calculator-keys">
+          <div className="container-row">
+            <button type="button" className="button-grey">7</button>
+            <button type="button" className="button-grey">8</button>
+            <button type="button" className="button-grey">9</button>
+            <button type="button" className="button-orange">x</button>
+          </div>
 
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
+          <div className="container-row">
+            <button type="button" className="button-grey">4</button>
+            <button type="button" className="button-grey">5</button>
+            <button type="button" className="button-grey">6</button>
+            <button type="button" className="button-orange">-</button>
+          </div>
 
-          <button type="button" className="operator">÷</button>
+          <div className="container-row">
+            <button type="button" className="button-grey">1</button>
+            <button type="button" className="button-grey">2</button>
+            <button type="button" className="button-grey">3</button>
+            <button type="button" className="button-orange">+</button>
+          </div>
 
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-
-          <button className="operator">x</button>
-
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-
-          <button className="operator">-</button>
-
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-
-          <button type="button" className="operator">+</button>
-
-          <button type="button" className="zero">0</button>
-
-          <button type="button">.</button>
-
-          <button type="button" className="operator">=</button>
+          <div className="third-row-bottom">
+            <button type="button" className="button-grey">0</button>
+            <button type="button" className="button-grey">.</button>
+            <button type="button" className="button-orange">=</button>
+          </div>
         </div>
+
       </div>
     );
   }
 }
-
-export default Calculator;
