7380220ce450a6e844a6b4638a56df9063bd560e
TayyabKM <109720981+TayyabKM@users.noreply.github.com>
2023-04-28 14:40:25 +0200

    Fix linter issues

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Calculator from "./components/calculatorPage/calculator";
-import DisplayQuote from "./components/calculatorPage/DisplayQuote";
-import HomePage from "./components/homePage/home";
-import Navbar from "./components/navbarHeading/navbar";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Calculator from './components/calculatorPage/calculator';
+import DisplayQuote from './components/calculatorPage/DisplayQuote';
+import HomePage from './components/homePage/home';
+import Navbar from './components/navbarHeading/navbar';
 
 function App() {
   return (
@@ -13,12 +13,12 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route
           path="/calculator"
-          element={
+          element={(
             <div>
               <Calculator />
               <DisplayQuote />
             </div>
-          }
+          )}
         />
       </Routes>
     </div>
