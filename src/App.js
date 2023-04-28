6fd0b8601b113546fd7d45418573c9b073073553
TayyabKM <109720981+TayyabKM@users.noreply.github.com>
2023-04-28 14:30:07 +0200

    Upgrade files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Calculator from './components/calculatorPage/calculator';
-import Quotes from './components/quotePage/quote';
+import DisplayQuote from './components/calculatorPage/DisplayQuote';
 import HomePage from './components/homePage/home';
 import Navbar from './components/navbarHeading/navbar';
-import './logic/calculate';
-import './logic/operate';
 
-export default function App() {
+function App() {
   return (
-    <div className="App">
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/calculator" element={<Calculator />} />
-          <Route path="/quote" element={<Quotes />} />
-        </Routes>
-      </div>
+    <div className="pp">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/calculator"
+          element={(
+            <div>
+              <Calculator />
+              <DisplayQuote />
+            </div>
+)}
+        />
+      </Routes>
     </div>
   );
 }
+
+export default App;
